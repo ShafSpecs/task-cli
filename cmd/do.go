bc735340f589da16df09b247659857e24a815162
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-11-18 19:37:12 +0100

    chore:Updated `do` command to reflect in `completed` bucket

--- cmd/do.go
+++ cmd/do.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"cli-task-manager/db"
 	"fmt"
+	"github.com/logrusorgru/aurora/v4"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +16,9 @@ var doCmd = &cobra.Command{
 	Short: "Marks a todo as complete",
 	Long:  `Every todo has to be done after a while. This marks yours as done`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Done")
+		err := db.RemoveFromBucket("tasks", args[0])
+		if err != nil {
+			fmt.Printf("Error occured: %s", aurora.Red(err))
+		}
 	},
 }
