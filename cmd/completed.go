0ab79142f958f43830db02723e2e74a0e08ee3d7
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-11-18 19:36:41 +0100

    chore:Added `completed` command

---
+++ cmd/completed.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"cli-task-manager/db"
+	"fmt"
+	"github.com/logrusorgru/aurora/v4"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(completedCmd)
+}
+
+var completedCmd = &cobra.Command{
+	Use:   "completed",
+	Short: "Shows completed tasks",
+	Long:  `View all your completed tasks and give yourself a pat on the back`,
+	Run: func(cmd *cobra.Command, args []string) {
+		items := db.ListBucketItems("completed")
+
+		fmt.Printf("You have finished the following tasks %s:\n", aurora.Magenta("today"))
+
+		for _, v := range items {
+			fmt.Printf("- %s\n", v)
+		}
+	},
+}
