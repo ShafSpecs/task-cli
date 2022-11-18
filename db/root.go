b6a8c4e58362eaf411351055c7a2e6b243258204
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-11-18 19:38:21 +0100

    done: Updated bucket interactions for more depth

--- db/root.go
+++ db/root.go
@@ -26,7 +26,9 @@ func AddToBucket(bucketName string, task string) error {
 	err := Db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucketName))
 
-		_ = b.Put([]byte(strconv.Itoa(bucketLength(b)+1)), []byte(task))
+		k, _ := b.NextSequence()
+
+		_ = b.Put([]byte(strconv.Itoa(int(k))), []byte(task))
 
 		return nil
 	})
@@ -38,7 +40,74 @@ func AddToBucket(bucketName string, task string) error {
 	return nil
 }
 
-func RemoveFromBucket(bucketName string, task string) {}
+func RemoveFromBucket(bucketName string, taskNumber string) error {
+	task, _ := strconv.Atoi(taskNumber)
+
+	err := Db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+
+		if bucketName == "tasks" {
+			c := b.Cursor()
+			current := 1
+
+			completed := tx.Bucket([]byte("completed"))
+
+			for k, _ := c.First(); current <= task; k, _ = c.Next() {
+				if current == task {
+					t := b.Get(k)
+					_ = completed.Put(k, t)
+
+					err := b.Delete(k)
+					if err != nil {
+						return err
+					}
+				}
+
+				current++
+			}
+
+			return nil
+		} else {
+			return nil
+		}
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteFromBucket(taskNumber string) error {
+	task, _ := strconv.Atoi(taskNumber)
+
+	err := Db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("tasks"))
+
+		c := b.Cursor()
+		current := 1
+
+		for k, _ := c.First(); current <= task; k, _ = c.Next() {
+			if current == task {
+				err := b.Delete(k)
+				if err != nil {
+					return err
+				}
+			}
+
+			current++
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func ListBucketItems(bucketName string) map[string]string {
 	items := make(map[string]string)
@@ -49,7 +118,7 @@ func ListBucketItems(bucketName string) map[string]string {
 		c := b.Cursor()
 
 		for k, v := c.First(); k != nil; k, v = c.Next() {
-			items[string(k[:])] = string(v)
+			items[string(k)] = string(v)
 		}
 
 		return nil
@@ -58,23 +127,13 @@ func ListBucketItems(bucketName string) map[string]string {
 	return items
 }
 
-func bucketLength(b *bolt.Bucket) int {
-	c := b.Cursor()
-	var sum int
-
-	for k, _ := c.First(); k != nil; k, _ = c.Next() {
-		sum++
-	}
-
-	return sum
-}
-
 func createBucket(name string) *bolt.Bucket {
 	var bucket *bolt.Bucket
 
 	_ = Db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(name))
 		if b == nil {
+
 			b, _ := tx.CreateBucket([]byte(name))
 			bucket = b
 		} else {
@@ -95,3 +154,14 @@ func CloseDB() error {
 
 	return nil
 }
+
+//func bucketLength(b *bolt.Bucket) int {
+//	c := b.Cursor()
+//	var sum int
+//
+//	for k, _ := c.First(); k != nil; k, _ = c.Next() {
+//		sum++
+//	}
+//
+//	return sum
+//}
