b44f1e812c0757b4f9af1b906fe38378d0dea23d
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-11-18 19:36:48 +0100

    chore:Added `rm` command

---
+++ cmd/rm.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"cli-task-manager/db"
+	"fmt"
+	"github.com/logrusorgru/aurora/v4"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(rmCmd)
+}
+
+var rmCmd = &cobra.Command{
+	Use:   "rm",
+	Short: "Deletes a todo task",
+	Long:  `Deletes a task forever! No one would know...`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := db.DeleteFromBucket(args[0])
+		if err != nil {
+			fmt.Printf("Error occured: %s", aurora.Red(err))
+		}
+	},
+}
